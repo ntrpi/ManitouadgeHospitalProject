7d5d37f44f65ab6d9b7ce1fbb7ad22d8f9019c53
Sandra Kupfer <kupfer.sandra.m@gmail.com>
2021-03-18 16:59:27 -0400

    Add ViewInvoice model.

---
+++ Manitouage1/Models/ViewModels/ViewInvoice.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Manitouage1.Models.ViewModels
+{
+    public class ViewInvoice
+    {
+        public class Totals
+        {
+            public decimal subTotal {
+                get; set;
+            }
+
+            public decimal taxes {
+                get; set;
+            }
+
+            public decimal total {
+                get; set;
+            }
+        }
+
+        public InvoiceDto invoiceDto {
+            get; set;
+        }
+
+        public IEnumerable<ProductDto> productDtos {
+            get; set;
+        }
+
+        private Totals totals;
+
+        public Totals getTotals()
+        {
+            if( totals == null ) {
+                decimal subTotal = 0;
+                decimal taxes = 0;
+                decimal total = 0;
+                foreach( ProductDto product in productDtos ) {
+                    subTotal += product.price;
+                    taxes += product.price * product.taxRate;
+                    total += subTotal + taxes;
+                }
+                totals = new Totals {
+                    subTotal = subTotal,
+                    taxes = taxes,
+                    total = total
+                };
+            }
+            return totals;
+        }
+
+        public decimal getTaxes()
+        {
+            decimal subTotal = 0;
+            foreach( ProductDto product in productDtos ) {
+                subTotal += product.price;
+            }
+            return subTotal;
+        }
+    }
+}
