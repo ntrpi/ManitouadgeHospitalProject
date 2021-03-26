a8702ee161fbc0514e404ae8a6aa08887e40fd48
AmandaDeannaElias <amanda.elias@live.ca>
2021-03-25 21:29:51 -0400

    pushing events

--- Manitouage1/Models/Event.cs
+++ Manitouage1/Models/Event.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Manitouage1.Models
 {
     public class Event
