6e8747e58982f676bcf4bca95fe696160e2175c8
AmandaDeannaElias <amanda.elias@live.ca>
2021-03-25 21:19:51 -0400

    commiting events

---
+++ Manitouage1/Models/Event.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Manitouage1.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId
+        {
+            get; set;
+        }
+        public string Title
+        {
+            get; set;
+        }
+        public string Description
+        {
+            get; set;
+        }
+        public DateTime DateTime
+        {
+            get; set;
+        }
+        public string Location
+        {
+            get; set;
+        }
+        public string Duration
+        {
+            get; set;
+        }
+        public string ContactPerson
+        {
+            get; set;
+        }
+    }
+}
