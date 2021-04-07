ca570dc3a03ab35969c95631b2f7c78e8068e38a
AmandaDeannaElias <amanda.elias@live.ca>
2021-04-06 21:55:26 -0400

    changes to databases

--- Manitouage1/Models/Event.cs
+++ Manitouage1/Models/Event.cs
@@ -46,4 +46,27 @@ namespace Manitouage1.Models
             get; set;
         }
     }
+    public class EventDto
+    {
+        [DisplayName("Event ID")]
+        public int EventId { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Event Title")]
+        public string Title { get; set; }
+
+        [DisplayName("Date and Time")]
+        public DateTime DateTime { get; set; }
+
+        [DisplayName("Location")]
+        public string Location { get; set; }
+
+        [DisplayName("Duration")]
+        public Decimal Duration { get; set; }
+
+        [DisplayName("Contact Personnel")]
+        public string ContactPerson { get; set; }
+    }
 }
