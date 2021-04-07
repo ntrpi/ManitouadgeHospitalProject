12a9dd0c8f702db02ed7350f6c740ed07d586066
AmandaDeannaElias <amanda.elias@live.ca>
2021-04-06 22:34:14 -0400

    adding events changes

---
+++ Manitouage1/Controllers/EventsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Script.Serialization;
+using Manitouage1.Models;
+using System.Diagnostics;
+
+namespace Manitouage1.Controllers
+{
+    public class EventsController : Controller
+    {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+        // GET: Events
+        static EventsController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            //change this to match your own local port number
+            client.BaseAddress = new Uri("https://localhost:44397/api/");
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Events/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Manitouage1/Controllers/EventsDataController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Manitouage1.Models;
+
+namespace Manitouage1.Controllers
+{
+    public class EventsDataController : ApiController
+    {
+        private ManitouageDbContext db = new ManitouageDbContext();
+
+        // GET: api/GetEvents
+        public IHttpActionResult GetEvents()
+        {
+            // Get the rows from the Questions table and put them in a List object.
+            List<Event> Events = db.events.ToList();
+
+            // Create a List object to hold the dtos.
+            List<EventDto> EventDtos = new List<EventDto> { };
+
+            // Convert each Question into a QuestionDto and put it in the list.
+            foreach (var Event in Events)
+            {
+                EventDto NewEvent = new EventDto
+                {
+                    // Set the dto properties.
+                    EventId = Event.EventId,
+                    Title = Event.Title,
+                    Description = Event.Description,
+                    DateTime = Event.DateTime,
+                    Location = Event.Location,
+                    Duration = Event.Duration,
+                    ContactPerson = Event.ContactPerson
+                };
+
+                // Add the dto to the list.
+                EventDtos.Add(NewEvent);
+            }
+
+            // Return the Ok http action result containing the dto list.
+            return Ok(EventDtos);
+        }
+        // GET: api/EventsData/5
+        [ResponseType(typeof(Event))]
+        public IHttpActionResult GetEvent(int id)
+        {
+            Event @event = db.events.Find(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@event);
+        }
+
+        // PUT: api/EventsData/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEvent(int id, Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != @event.EventId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(@event).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EventsData
+        [ResponseType(typeof(Event))]
+        public IHttpActionResult PostEvent(Event @event)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.events.Add(@event);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = @event.EventId }, @event);
+        }
+
+        // DELETE: api/EventsData/5
+        [ResponseType(typeof(Event))]
+        public IHttpActionResult DeleteEvent(int id)
+        {
+            Event @event = db.events.Find(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            db.events.Remove(@event);
+            db.SaveChanges();
+
+            return Ok(@event);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EventExists(int id)
+        {
+            return db.events.Count(e => e.EventId == id) > 0;
+        }
+    }
+}
